c804f4fdf22bcdbacd501afa6c0f16340482d7c0
SoJDev | Hoàng Lâm <lhlam2515@gmail.com>
2025-11-24 15:22:13 +0700

    fix(backend): update ESLint and TypeScript configurations

--- apps/backend/eslint.config.mjs
+++ apps/backend/eslint.config.mjs
@@ -1,4 +1,15 @@
 import { config } from "@repo/eslint-config";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig(config);
+export default defineConfig({
+  ...config,
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+      node: true,
+    },
+  },
+});
