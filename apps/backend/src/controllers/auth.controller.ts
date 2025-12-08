f637185e9947743cc78d6f5e236b3e220fb3a641
SoJDev | Hoàng Lâm <lhlam2515@gmail.com>
2025-12-08 08:20:52 +0700

    fix(backend): correct the path of refresh token in cookie

--- apps/backend/src/controllers/auth.controller.ts
+++ apps/backend/src/controllers/auth.controller.ts
@@ -51,7 +51,7 @@ export const login = asyncHandler(
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        path: "/api/auth/refresh-token",
+        path: "/api/v1/auth/refresh-token",
       });
     }
 
@@ -86,7 +86,7 @@ export const refreshToken = asyncHandler(
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        path: "/api/auth/refresh-token",
+        path: "/api/v1/auth/refresh-token",
       });
     }
 
