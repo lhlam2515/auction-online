d28eaffae7b540ad03b00d2d7bf1c19c821e97ff
SoJDev | Hoàng Lâm <lhlam2515@gmail.com>
2025-12-10 20:50:41 +0700

    refactor(user): simplify the retrieval logic in controller

--- apps/backend/src/controllers/user.controller.ts
+++ apps/backend/src/controllers/user.controller.ts
@@ -58,31 +58,21 @@ export const changePassword = asyncHandler(
 );
 
 export const getPublicProfile = asyncHandler(
-  async (req: AuthRequest, res: Response, next: NextFunction) => {
-    // TODO: Get public profile of another user
-    if (!req.user || !req.user.id) {
-      throw new NotImplementedError("User not authenticated");
-    }
-    const publicID = req.params.id;
-    const publicProfile = await userService.getById(publicID);
-    if (!publicProfile) {
-      throw new NotImplementedError("Public profile not found");
-    }
+  async (req: AuthRequest, res: Response, _next: NextFunction) => {
+    const { id: publicId } = req.params;
+
+    const publicProfile = await userService.getById(publicId);
+
     return ResponseHandler.sendSuccess<PublicProfile>(res, publicProfile);
   }
 );
 
 export const getRatingSummary = asyncHandler(
-  async (req: AuthRequest, res: Response, next: NextFunction) => {
-    // TODO: Get user rating summary
-    if (!req.user || !req.user.id) {
-      throw new NotImplementedError("User not authenticated");
-    }
-    const userID = req.params.id;
+  async (req: AuthRequest, res: Response, _next: NextFunction) => {
+    const { id: userID } = req.params;
+
     const ratingSummary = await userService.getById(userID);
-    if (!ratingSummary) {
-      throw new NotImplementedError("Rating summary not found");
-    }
+
     return ResponseHandler.sendSuccess<UserRatingSummary>(res, {
       averageRating: ratingSummary.ratingScore,
       totalRatings: ratingSummary.ratingCount,
@@ -91,65 +81,52 @@ export const getRatingSummary = asyncHandler(
 );
 
 export const toggleWatchlist = asyncHandler(
-  async (req: AuthRequest, res: Response, next: NextFunction) => {
-    // TODO: Add/Remove product from watchlist
-    if (!req.user || !req.user.id) {
-      throw new NotImplementedError("User not authenticated");
-    }
-    const userId = req.user.id;
+  async (req: AuthRequest, res: Response, _next: NextFunction) => {
+    const { id: userId } = req.user!;
     const productId = req.params.productId;
+
     const exists = await userService.checkInWatchlist(userId, productId);
     if (exists) {
       await userService.removeFromWatchlist(userId, productId);
-      return ResponseHandler.sendSuccess<string>(
-        res,
-        "Product removed from watchlist"
-      );
+      return ResponseHandler.sendSuccess(res, {
+        message: "Product removed from watchlist",
+      });
     } else {
       await userService.addToWatchlist(userId, productId);
-      return ResponseHandler.sendSuccess<string>(
-        res,
-        "Product added to watchlist"
-      );
+      return ResponseHandler.sendSuccess(res, {
+        message: "Product added to watchlist",
+      });
     }
   }
 );
 
 export const getWatchlist = asyncHandler(
-  async (req: AuthRequest, res: Response, next: NextFunction) => {
-    // TODO: Get user's watchlist
-    if (!req.user || !req.user.id) {
-      throw new NotImplementedError("User not authenticated");
-    }
-    const userId = req.user.id;
+  async (req: AuthRequest, res: Response, _next: NextFunction) => {
+    const { id: userId } = req.user!;
+
     const watchlist = await userService.getWatchlist(userId);
+
     return ResponseHandler.sendSuccess<Product[]>(res, watchlist);
   }
 );
 
 export const getBiddingHistory = asyncHandler(
-  async (req: AuthRequest, res: Response, next: NextFunction) => {
-    // TODO: Get user's bidding history
-    if (!req.user || !req.user.id) {
-      throw new NotImplementedError("User not authenticated");
-    }
-    const userId = req.user.id;
+  async (req: AuthRequest, res: Response, _next: NextFunction) => {
+    const { id: userId } = req.user!;
+
     const biddingHistory = await userService.getBiddingHistory(userId);
+
     return ResponseHandler.sendSuccess<Bid[]>(res, biddingHistory);
   }
 );
 
 export const requestUpgrade = asyncHandler(
-  async (req: AuthRequest, res: Response, next: NextFunction) => {
-    const body = req.body as UpgradeRequestData;
-    // TODO: Request upgrade to Seller role
-    if (!req.user || !req.user.id) {
-      throw new NotImplementedError("User not authenticated");
-    }
-    const userId = req.user.id;
-    const reason = body.reason;
+  async (req: AuthRequest, res: Response, _next: NextFunction) => {
+    const { id: userId } = req.user!;
+    const { reason } = req.body as UpgradeRequestData;
 
     const request = await userService.requestUpgradeToSeller(userId, reason);
-    return ResponseHandler.sendSuccess<any>(res, request);
+
+    return ResponseHandler.sendSuccess(res, request);
   }
 );
