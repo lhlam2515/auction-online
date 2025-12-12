61736dda18f817531bbfc814f09c88998c772393
SoJDev | Hoàng Lâm <lhlam2515@gmail.com>
2025-12-12 10:48:16 +0700

    feat(layout): implement Navbar component with category fetching and user authentication links

--- apps/frontend/app/routes/_root/layout.tsx
+++ apps/frontend/app/routes/_root/layout.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router";
 
+import Navbar from "@/components/layout/navbar";
+
 export default function RootLayout() {
   return (
     <div className="root-layout">
-      {/* Add Header/Sidebar specific to this layout */}
-      <Outlet />
+      <Navbar />
+
+      <div className="container mx-auto min-h-screen">
+        <Outlet />
+      </div>
     </div>
   );
 }
